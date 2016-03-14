e13601f556565aa7d1919a89ff55f8145abc88dd
Roisko Pietu-Pekka <roiskop1@kosh.org.aalto.fi>
2016-03-14 15:01:40 +0200

    unit tests added

--- jaakaappi.py
+++ jaakaappi.py
@@ -18,8 +18,16 @@ class JaaKaappi:
     print("Jääkaapin sisältö:")
     for aines in self.ruokaAineet:
       print( aines)
-  def lataaJaakaappi( self, tiedosto):
-    pass
+  def lataaJaakaappi( self, buf):
+    line = buf.readline()
+    line_s = line.split(' ')
+    if (line_s[0] != 'Jääkaappitiedosto'):
+      raise BufferError('File format not jääkaappi-format')
+      return False
+    while (line):
+      print( line)
+      line = buf.readline()
+
 
 """ UNIT TESTS """
 class Test( unittest.TestCase):
@@ -27,16 +35,24 @@ class Test( unittest.TestCase):
   def test_testing( self):
     self.assertEqual( True, True)
 
+  #test header
+  def test_header( self):
+    test_data = u"Moromoro versio 1.0\n"
+    self.input_file = StringIO(test_data)
+    kaappi = JaaKaappi()
+    self.assertRaises(BufferError, kaappi.lataaJaakaappi,self.input_file)
+
+
   def test_loading( self):
-    test_data = u"Jääkaappitiedosto versio 1.0"\
-      + u"# Matin jääkaappi"\
-      + u"JÄÄKAAPIN SISÄLTÖ"\
+    test_data = u"Jääkaappitiedosto versio 1.0\n"\
+      + u"# Matin jääkaappi\n"\
+      + u"JÄÄKAAPIN SISÄLTÖ\n"\
       + u"Maito\t1\tpurkki\ta\t10dl\tVL\3.4.2016"
     self.input_file = StringIO(test_data)
     kaappi = JaaKaappi()
     try:
       kaappi.lataaJaakaappi(self.input_file)
-    except CorruptedChessFileError:
+    except IOError:
       self.fail("Loading a correctly structured file caused an exception")
     self.input_file.close()
 
